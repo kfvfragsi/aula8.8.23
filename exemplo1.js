32f255945e5245e6b07cd60a9cc5a6d71974297a
Victor <139802305+kfvfragsi@users.noreply.github.com>
2023-08-08 16:17:32 -0300

    Add files via upload

---
+++ exemplo1.js
@@ -0,0 +1,10 @@
+const carro = {
+    marca: "FIAT",
+    modelo:"UNO",
+    ano:"1989",
+    cor:"Prata",
+    placa:"RPQ895",
+    portas:"Quatro",
+    motor: function(){alert('Galooo')}
+};
+[console.log(carro.motor())]
---
+++ exemplo2.js
@@ -0,0 +1,14 @@
+const programacao = {
+    descanso:"tempo relacionado a descanso",
+    trabalho:"tempo relacionado ao tabalho",
+    estudo:"tempo relaciionado aos estudos",
+    domestico:"tempo relacionado a organização de casa",
+    lazer:"tempo relacionado a lazer/esportes",
+    
+    diario: function(){alert('8h de Descanso')},
+    diarioa: function(){alert('8h de trabalho')},
+    diariob: function(){alert('4h de Estudo')},
+    diarioc: function(){alert('3h Organização')},
+    diariod: function(){alert('1h lazer/esporte')},
+};
+console.log(programacao.diario()),(programacao.diarioa()),(programacao.diariob()),(programacao.diarioc()),(programacao.diariod())
